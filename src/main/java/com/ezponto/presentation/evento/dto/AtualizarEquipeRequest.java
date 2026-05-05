1c78f5f38edc2445f84be733c85510d49017570b
Kayky Morais <kaykygithub@gmail.com>
2026-05-04 22:08:19 -0300

    fix editar

--- src/main/java/com/ezponto/presentation/evento/dto/AtualizarEquipeRequest.java
+++ src/main/java/com/ezponto/presentation/evento/dto/AtualizarEquipeRequest.java
@@ -1,10 +1,12 @@
 package com.ezponto.presentation.evento.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record AtualizarEquipeRequest(
+        @JsonProperty("employeeIds")
         @NotNull(message = "Lista de membros é obrigatória")
         List<Long> funcionarioIds
 ) {}
